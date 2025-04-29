6f0dafaa8d7d99fd0c4e032303be824ea0e07ac2
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-04-29 10:38:46 -0300

    :sparkles: feat: Adicionado método para editar um Product

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 
 def home(request):
@@ -10,3 +10,14 @@ def save(request):
     Product.objects.create(name=name)
     products = Product.objects.all()
     return render(request, "index.html", {"products": products})
+
+def edit(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, "update.html", {"product": product})
+
+def update(request, id):
+    name = request.POST.get("name")
+    product = Product.objects.get(id=id)
+    product.name = name
+    product.save()
+    return redirect(home)
