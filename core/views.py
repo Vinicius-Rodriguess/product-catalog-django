a409cefe83843a77210f251d0e79489ef2c3bd67
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-04-29 10:20:52 -0300

    :sparkles: feat: Adicionado método para salvar um Product

--- core/views.py
+++ core/views.py
@@ -4,3 +4,9 @@ from .models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, "index.html", {"products": products})
+
+def save(request):
+    name = request.POST.get("name")
+    Product.objects.create(name=name)
+    products = Product.objects.all()
+    return render(request, "index.html", {"products": products})
